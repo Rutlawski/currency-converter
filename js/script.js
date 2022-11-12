f50eb79e873a08d56260e661fff6d00c014aa751
Marek Ostrowski <rutlawski2@wp.pl>
2022-11-12 11:40:54 +0100

    Uporządkowanie funkcji w Javascripcie

--- js/script.js
+++ js/script.js
@@ -1,51 +1,44 @@
-console.log("Hello World!")
+{
+    const Przywitanie = () => {
+        console.log("Hello World!")
+    };
 
-let formElement = document.querySelector(".js-form");
-let ammountElement = document.querySelector(".js-ammount");
-let resultElement = document.querySelector(".js-result");
-let dollarElement = document.querySelector(".js-dollar");
-let euroElement = document.querySelector(".js-euro");
-let poundElement = document.querySelector(".js-pound");
-let buttonElement = document.querySelector(".js-fieldset__button");
-
-buttonElement.addEventListener("click", () => {
-    let result;
-
-    result = (" 0");
-
-    resultElement.innerText = result;
-})
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let result;
-
-    if (dollarElement.checked === true) {
-
-        let dollarElement = 0.21;
-
-
-        result = (ammountElement.value * dollarElement);
+    const Init = () => {
+        const formElement = document.querySelector(".js-form");
+        const buttonElement = document.querySelector(".js-fieldset__button");
+        formElement.addEventListener("submit", onSubmitElement);
+        buttonElement.addEventListener("click", onClickReset);
     }
 
-    else if (euroElement.checked === true) {
-
-        let euroElement = 0.21;
-
-        result = (ammountElement.value * euroElement);
+    const onClickReset = () => {
+        const resultElement = document.querySelector(".js-result");
+        result = ("0");
+        resultElement.innerText = result;
     }
 
-    else if (poundElement.checked === true) {
-
-        let poundElement = 0.18;
-
-        result = (ammountElement.value * poundElement);
+    const onSubmitElement = (event) => {
+        event.preventDefault();
+        const dollarElement = document.querySelector(".js-dollar");
+        const euroElement = document.querySelector(".js-euro");
+        const poundElement = document.querySelector(".js-pound");
+        const resultElement = document.querySelector(".js-result");
+        const ammountElement = document.querySelector(".js-ammount");
+        if (dollarElement.checked === true) {
+            const dollarElement = 0.21;
+            result = (ammountElement.value * dollarElement);
+        }
+        else if (euroElement.checked === true) {
+            const euroElement = 0.21;
+            result = (ammountElement.value * euroElement);
+        }
+        else if (poundElement.checked === true) {
+            const poundElement = 0.18;
+            result = (ammountElement.value * poundElement);
+        }
+        resultElement.innerText = result;
     }
 
-    resultElement.innerText = result;
+    Przywitanie();
+    Init();
 
-})
-
-
-
-
+}
