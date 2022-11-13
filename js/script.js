131b537f9e58b18fdc70e5d430d5d30ba0120c07
Marek Ostrowski <rutlawski2@wp.pl>
2022-11-13 15:11:07 +0100

    * Backround edits * Js functions edits * Functionality fix

--- js/script.js
+++ js/script.js
@@ -3,39 +3,43 @@
         console.log("Hello World!")
     };
 
-    const Init = () => {
-        const formElement = document.querySelector(".js-form");
-        const buttonElement = document.querySelector(".js-fieldset__button");
-        formElement.addEventListener("submit", onSubmitElement);
-        buttonElement.addEventListener("click", onClickReset);
-    }
-
     const onClickReset = () => {
         const resultElement = document.querySelector(".js-result");
         result = (" 0");
         resultElement.innerText = result;
     }
 
-    const onSubmitElement = (event) => {
-        event.preventDefault();
+    const getRate = () => {
         const dollarElement = document.querySelector(".js-dollar");
         const euroElement = document.querySelector(".js-euro");
         const poundElement = document.querySelector(".js-pound");
-        const resultElement = document.querySelector(".js-result");
-        const ammountElement = document.querySelector(".js-ammount");
-        resultElement.innerText = result;
+
         if (dollarElement.checked === true) {
-            const dollarElement = 0.21;
-            result = (ammountElement.value * dollarElement);
+            return 0.21;
         }
         else if (euroElement.checked === true) {
-            const euroElement = 0.21;
-            result = (ammountElement.value * euroElement);
+            return 0.22;
         }
         else if (poundElement.checked === true) {
-            const poundElement = 0.18;
-            result = (ammountElement.value * poundElement);
+            return 0.18;
         }
+    }
+
+    const onSubmitElement = (event) => {
+        event.preventDefault();
+        const resultElement = document.querySelector(".js-result");
+        const ammountElement = document.querySelector(".js-ammount")
+        const rate = getRate();
+
+        resultElement.innerText = (+ammountElement.value * rate);
+    }
+
+
+    const Init = () => {
+        const formElement = document.querySelector(".js-form");
+        const buttonElement = document.querySelector(".js-fieldset__button");
+        formElement.addEventListener("submit", onSubmitElement);
+        buttonElement.addEventListener("click", onClickReset);
     }
 
     Przywitanie();
